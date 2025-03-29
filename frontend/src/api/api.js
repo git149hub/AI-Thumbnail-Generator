990e7e265070f6945785e8e45764a1c58108a8da
git149hub <202775476+git149hub@users.noreply.github.com>
2025-03-29 12:42:18 +0530

    api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL = "http://localhost:5000/api";
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://thumbnail-generator-u0ue.onrender.com/api";
 
 // Function to generate a new thumbnail
 export const generateThumbnail = async (formData) => {
