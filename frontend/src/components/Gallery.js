b2431119333b2dd046006f48496d1b135993a511
git149hub <202775476+git149hub@users.noreply.github.com>
2025-03-29 15:23:42 +0530

    Gallery.js

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -11,7 +11,7 @@ const Gallery = () => {
     const fetchGallery = async () => {
       try {
         // const response = await axios.get("http://localhost:5000/api/thumbnails/gallery");
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/thumbnails/gallery`);
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/thumbnails/gallery`);
         setThumbnails(response.data.thumbnails);
       } catch (error) {
         console.error("Error fetching gallery images:", error);
